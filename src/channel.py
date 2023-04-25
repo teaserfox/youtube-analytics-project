7b7dc293c0b778e8a3e70990dd53f170ac4bbe80
Kokorina Tatyana <teaserfox@gmail.com>
2023-04-25 15:28:13 +0300

    'оформлены методы'

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,6 @@
+import os
+import json
+from googleapiclient.discovery import build
 
 
 class Channel:
@@ -6,7 +9,11 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        api_key: str = os.getenv('YT_API_KEY')
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = self.youtube.channels().list(id=self.channel_id, part="snippet,statistics").execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
