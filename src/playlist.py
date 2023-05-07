d341f4c14baf31bde8f1292cf0daed22ab30cdd0
Kokorina Tatyana <teaserfox@gmail.com>
2023-05-07 20:59:56 +0300

    'test'

--- src/playlist.py
+++ src/playlist.py
@@ -5,25 +5,21 @@ from googleapiclient.discovery import build
 
 
 class PlayList:
+
     api_key: str = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, playlist_id: str) -> None:
-        """
-        :id плейлиста
-        :название плейлиста
-        :ссылка на плейлист
-        """
         self.__playlist_id = playlist_id
         self.__playlist_videos = self.youtube.playlistItems().list(playlistId=playlist_id, part='contentDetails',
                                                                    maxResults=50, ).execute()
-        self.__duration = self.youtube.videos().list(part='contentDetails,statistics',
-                                                     id=','.join(self.__playlist_id)).execute()
+        self.__video_ids = [video['contentDetails']['videoId'] for video in self.__playlist_videos['items']]
+
         self.__video = self.youtube.videos().list(part='contentDetails,statistics',
-                                                  id=','.join(self.__playlist_id)).execute()
-        self.__playlist_videos = self.youtube.playlists().list(part='snippet',
-                                                               id=self.__playlist_id, ).execute()
-        self.title = self.__playlist_videos['items'][0]['snippet']['title']
+                                                  id=','.join(self.__video_ids)).execute()
+        self.__playlist_info = self.youtube.playlists().list(part='snippet',
+                                                             id=self.__playlist_id, ).execute()
+        self.title = self.__playlist_info['items'][0]['snippet']['title']
         self.url = f'https://www.youtube.com/playlist?list={self.__playlist_id}'
 
     @property
@@ -37,19 +33,11 @@ class PlayList:
         return total
 
     def show_best_video(self):
+        best_video = ""
         likes = 0
-
-        for video_id in self.__playlist_videos:
-
-            video_request = self.youtube.videos().list(
-                part='statistics',
-                id=video_id
-            ).execute()
-
-            like_count = video_request['items'][0]['statistics']['likeCount']
-
+        for video in self.__video["items"]:
+            like_count = video['statistics']['likeCount']
             if int(like_count) > likes:
                 likes = int(like_count)
-                best_video = f"https://youtu.be/{video_request['items'][0]['id']}"
-
+                best_video = f"https://youtu.be/{video['id']}"
         return best_video
